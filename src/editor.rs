787859e491740b60116ebf948bda968cb0645998
Ayush Gupta <ayush4002gupta@gmail.com>
2025-03-14 23:09:34 +0530

    feat : initialize basic text editor with functionality and input handling

---
+++ src/editor.rs
@@ -0,0 +1,77 @@
+
+
+use crossterm::event::{KeyEventKind, KeyEventState};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use std::io::stdout;
+pub struct Editor {
+
+    should_quit: bool,
+
+}
+
+impl Editor{
+
+    pub fn new() -> Self {
+        Editor{
+            should_quit: false,
+        }
+    }
+    
+    pub fn run(&mut self){
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+    fn initialize() -> Result<(), std::io::Error>{
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+  
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+    fn repl(&mut self) -> Result<(), std::io::Error>{
+        loop { 
+            
+            let event = read()?;
+            _ = self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+ 
+        }
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self , event : &Event)-> Result<(), std::io::Error>{
+        if let Key(KeyEvent{kind:_, modifiers, code, state:_}) = event {
+          match code {
+              Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                self.should_quit = true;
+                
+              }
+              _ => (),
+              
+          }
+        }
+        Ok(())
+        
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            _ = Self::clear_screen();
+            print!("Goodbye.\r\n");
+        } 
+        Ok(())
+        
+    }
+}
